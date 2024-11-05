2ccc512d45c2ba4688315a9290a73048bf6ff84e
! Jajou <jahelgithub64@gmail.com>
2024-11-05 21:40:56 +0100

    Adding some css and js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,7 @@ const discordHover = document.querySelector(".link-profile-discord");
 const youtubeHover = document.querySelector(".link-profile-youtube");
 const twitchHover = document.querySelector(".link-profile-twitch");
 
-const audio = document.getElementById("audio");
-const progressBar = document.getElementById("progress-bar");
-const playPauseBtn = document.getElementById("play-pause-btn");
-const progressContainer = document.querySelector(".progress-container");
-const currentTimeEl = document.getElementById("current-time");
-const durationEl = document.getElementById("duration");
+const audio = document.querySelector('audio')
 
 const descriptions = ["Youtuber & Web Dev", "I Love Play Video Games"];
 
@@ -38,40 +33,6 @@ setupHoverEffect(discordHover, "discord-hover");
 setupHoverEffect(youtubeHover, "youtube-hover");
 setupHoverEffect(twitchHover, "twitch-hover");
 
-function playPause() {
-  if (audio.paused) {
-    audio.play();
-    playPauseBtn.textContent = "Pause";
-  } else {
-    audio.pause();
-    playPauseBtn.textContent = "Play";
-  }
-}
-
-function updateProgress() {
-  const progressPercent = (audio.currentTime / audio.duration) * 100;
-  progressBar.style.width = progressPercent + "%";
-  currentTimeEl.textContent = formatTime(audio.currentTime);
-}
-
-audio.addEventListener("loadedmetadata", () => {
-  durationEl.textContent = formatTime(audio.duration);
-});
-
-function setProgress(event) {
-  const width = progressContainer.clientWidth;
-  const clickX = event.offsetX;
-  const duration = audio.duration;
-
-  audio.currentTime = (clickX / width) * duration;
-}
-
-function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time % 60);
-  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-}
-
-playPauseBtn.addEventListener("click", playPause);
-audio.addEventListener("timeupdate", updateProgress);
-progressContainer.addEventListener("click", setProgress);
+window.addEventListener('click', () => {
+  audio.play()
+})
